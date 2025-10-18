3ccb2f53489777617cee69ee38734c9fb3c5d055
Himanshu pant <himanshupant2315@gmail.com>
2025-10-18 14:42:06 +0530

    Implement search and suggestions for listings
    Added search and suggestions routes for listings.

--- routes/listing.js
+++ routes/listing.js
@@ -14,7 +14,37 @@ const upload = multer({ storage })
 router.route("/")
 .get( wrapAsync(listingController.index))  //index route
 .post( isLoggedIn,upload.single("listing[image]"), validateListing ,wrapAsync(listingController.createListings)) //create route
+ // search box
+router.get("/search",async (req,res)=>{
+    const query =req.query.q;
+    if (!query) return res.redirect("/listings");
+     const allListings = await Listing.find({
+        $or:[
+            {title :{$regex :query,$options:"i"}},
+            {location:{$regex:query,$options:"i"}},
+        ],
+    });
+    res.render("listings/index",{allListings});
+});
+router.get("/search/suggestions",async(req,res)=>{
+    const query =req.query.q;
+    if (!query) {
+        return res.json([]);
+    }
+ 
+    const listings = await Listing.find({
+        $or:[
+            {title :{$regex :query,$options:"i"}},
+            {location:{$regex:query,$options:"i"}},
+        ],
+    }).limit(5);
 
+    const suggestions = listings.map(I=>({
+        title:I.title,
+        location:I.title,
+    }));
+    res.json(suggestions);
+});
 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
